267f6ff4e91db26dfa5d0ab3567781b6c1c59169
diegojrodriguess <diego.rodrigues@ges.inatel.br>
2024-05-06 23:15:16 -0300

    apagando o que estava em command.js por conta de ser um comando de outro projeto

--- cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('login', (username,password) =>{
-    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
-    cy.get('#username').type(username)
-    cy.get('#password').type(password)
-    cy.get('.btn-primary').click()
-})
